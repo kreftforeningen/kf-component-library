f373030a7a12c74c0e75ea41d3ec56ed6a23cfdb
Asbjørn Ness <asbjorn.ness@gmail.com>
2025-06-12 13:39:42 +0200

    Add dropdown menu, input, label, select and textarea

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -1,3 +1,8 @@
 //src/lib/main.ts
 import "../style.css";
 export { Button } from "../button";
+export { DropdownMenu } from "../dropdown-menu";
+export { Input } from "../input";
+export { Label } from "../label";
+export { Select } from "../select";
+export { Textarea } from "../textarea";
