bdf17a9e9986625d884778850c563a8fe8385403
Asbjørn Ness <asbjorn.ness@gmail.com>
2025-06-12 13:27:39 +0200

    init

---
+++ src/lib/main.ts
@@ -0,0 +1,3 @@
+//src/lib/main.ts
+import "../style.css";
+export { Button } from "../button";
---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import tailwindcss from "@tailwindcss/vite";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import react from "@vitejs/plugin-react";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [
+    dts({ include: ["src/lib"] }), // Generate .d.ts files
+    react(), // Enable React
+    tailwindcss(), // Enable Tailwind
+    cssInjectedByJsPlugin(), // Inject CSS into JS
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/lib/main.ts"), // Library entry point
+      formats: ["es", "cjs"], // Output formats
+    },
+    rollupOptions: {
+      // External dependencies that shouldn't be bundled
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        assetFileNames: "assets/[name].[extname]",
+        entryFileNames: "[name].[format].js",
+      },
+    },
+  },
+});
