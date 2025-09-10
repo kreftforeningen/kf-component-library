3e819700e681b5012c1d3ed6395387fd41026595
Asbjørn Ness <asbjorn.ness@gmail.com>
2025-09-10 18:48:53 +0200

    New styling for tabs

--- src/tabs/index.tsx
+++ src/tabs/index.tsx
@@ -10,7 +10,7 @@ function Tabs({
   return (
     <TabsPrimitive.Root
       data-slot="tabs"
-      className={cn("flex flex-col gap-2", className)}
+      className={cn("flex flex-col p-2 bg-gray-500 rounded-xl", className)}
       {...props}
     />
   );
@@ -24,7 +24,7 @@ function TabsList({
     <TabsPrimitive.List
       data-slot="tabs-list"
       className={cn(
-        "bg-grey-100 text-muted-foreground inline-flex h-12 w-fit items-center justify-center gap-1 rounded-xl p-1",
+        "text-muted-foreground inline-flex h-12 w-fit items-center justify-center gap-1 ml-6",
         className
       )}
       {...props}
@@ -40,7 +40,7 @@ function TabsTrigger({
     <TabsPrimitive.Trigger
       data-slot="tabs-trigger"
       className={cn(
-        "data-[state=active]:bg-grey-200 dark:data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/10 text-foreground dark:text-foreground inline-flex h-[calc(100%)] flex-1 items-center justify-center gap-1.5 rounded-xl px-6 py-2 text-md  whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 data-[state=active]:hover:cursor-default hover:cursor-pointer hover:bg-grey-200 dark:hover:bg-input/10 font-semibold",
+        "data-[state=active]:bg-gray-100 dark:data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/10 data-[state=active]:text-foreground text-background dark:text-foreground inline-flex h-[calc(100%)] flex-1 items-center justify-center gap-1.5 rounded-t-xl px-6 py-2 text-md  whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 data-[state=active]:hover:cursor-default hover:cursor-pointer hover:bg-gray-300 hover:text-gray-950 dark:hover:bg-input/10 font-semibold",
         className
       )}
       {...props}
@@ -56,7 +56,7 @@ function TabsContent({
     <TabsPrimitive.Content
       data-slot="tabs-content"
       className={cn(
-        "flex-1 outline-none bg-grey-100 dark:bg-input/10 rounded-b-md rounded-xl p-6 w-full border-2 border-grey-200",
+        "flex-1 outline-none bg-gray-100 dark:bg-input/10 rounded-xl p-6 w-full",
         className
       )}
       {...props}
